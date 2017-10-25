0ece210ef7f105667d2df40a8d8231a9e2b46dd4
jpsberlin <jpsberlin@hotmail.com>
2017-10-25 15:24:57 +0200

    Create asana.js

---
+++ day2/asana.js
@@ -0,0 +1,25 @@
+module.exports = class Asana {
+	constructor(name, time, beginner) {
+		this.name = name;
+		this.time = time;
+		this.beginner = beginner;
+      
+	}
+}
+
+module.exports = class Pranayama {
+	constructor(name, time, beginner) {
+		this.name = name;
+		this.time = time;
+		this.beginner = beginner;
+      
+	}
+}
+module.exports = class Meditation {
+	constructor(name, time, beginner) {
+		this.name = name;
+		this.time = time;
+		this.beginner = beginner;
+      
+	}
+}
