e61f0d717a41f0881eadf8979ddd046fba419870
jpsberlin <jpsberlin@hotmail.com>
2017-10-26 16:55:48 +0200

    Update index.js

--- day2/index.js
+++ day2/index.js
@@ -1,4 +1,6 @@
 const Asana = require('./asana');
+const Pranayama = require('./pranayama');
+const Meditation = require('./meditation');
 const Database = require('./database');
 
 const red = require('ansi-red');
@@ -22,13 +24,17 @@ var pranayama1 = new Pranayama ('SaTaNaMa', '1 minute', 'yes');
 var pranayama2 = new Pranayama ('Chandra Pran', '5 minute', 'yes');
 
 var meditation1 = new Meditation ('SaTaNaMa', '7 minute', 'yes');
-
+ 
 var meditation2 = new Meditation ('Gong Meditation', '11 minute', 'yes');
 var meditation3 = new Meditation ('Heart Meditation', '5 minute', 'yes');
 
 console.log(asana1.name);
 
-console.log(asana1.time);
+console.log(asana2.time);
 
+console.log(asana3.name);
 
+console.log(pranayama1.name);
 
+console.log(meditation1.beginner);
+console.log(meditation2.name);
