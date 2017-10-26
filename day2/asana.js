e6aabdee5b68e8071c8a12ea4c5c32d9b4752551
jpsberlin <jpsberlin@hotmail.com>
2017-10-26 16:53:42 +0200

    Update asana.js

--- day2/asana.js
+++ day2/asana.js
@@ -7,19 +7,3 @@ module.exports = class Asana {
 	}
 }
 
-module.exports = class Pranayama {
-	constructor(name, time, beginner) {
-		this.name = name;
-		this.time = time;
-		this.beginner = beginner;
-      
-	}
-}
-module.exports = class Meditation {
-	constructor(name, time, beginner) {
-		this.name = name;
-		this.time = time;
-		this.beginner = beginner;
-      
-	}
-}
