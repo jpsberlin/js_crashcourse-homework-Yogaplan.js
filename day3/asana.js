bd86e97a14388ebd3765090fd04f9be656113ccb
jpsberlin <jpsberlin@hotmail.com>
2017-11-02 11:41:54 +0100

    Create asana.js

---
+++ day3/asana.js
@@ -0,0 +1,11 @@
+module.exports = class Asana {
+	constructor(name, time, beginner) {
+		this.name = name;
+		this.time = time;
+		this.beginner = beginner;
+      
+	}
+    static create(obj) {
+        return new Asana(obj.name.time.beginner);
+    }
+}
