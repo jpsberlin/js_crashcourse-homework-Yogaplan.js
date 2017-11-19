b4e46a0156492bd5f1dcb68fe4a53a7f55ecfacd
jpsberlin <jpsberlin@hotmail.com>
2017-11-19 14:13:23 +0100

    Create asana.js

---
+++ day6/routes/asana.js
@@ -0,0 +1,44 @@
+
+const express = require('express')
+const router = express.Router()
+
+const AsanaService = require('../services/asana-service')
+
+router.get('/', async (req, res, next) => {
+    res.send(await AsanaService.findAll())
+})
+
+router.get('/all', async (req, res, next) => {
+    const exercise = await AsanaService.findAll()
+    res.render('exercise-list', {exercise})
+})
+
+router.get('/:id', async (req, res, next) => {
+    const asana = await AsanaService.find(req.params.id)
+
+    res.render('asana-detail', {asana})
+})
+
+router.post('/', async (req, res, next) => {
+    const asana = await AsanaService.add(req.body)
+
+    res.send(asana)
+})
+
+router.post('/:id/yogaset', async (req, res, next) => {
+    const asana = await AsanaService.find(req.params.id)
+    const target = await AsanaService.find(req.body.targetId)
+
+    asana.yogaset.addToSet(target)
+    const updatedAsana = await asana.save()
+    res.send(updatedAsana)
+});
+
+router.delete('/:id', async (req, res, next) => {
+    await AsanaService.del(req.params.id)
+
+    res.send('ok!')
+})
+
+module.exports = router
+
